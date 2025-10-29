ba5f48ff32f5064d3e11b1fe1f98a75c81210f9b
okamonster <okamonster0615@gmail.com>
2025-10-29 17:40:27 +0900

    feat(web): add react-icons dependency and integrate icons in TopActions component

--- apps/web/src/features/top/components/TopActions/index.tsx
+++ apps/web/src/features/top/components/TopActions/index.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { LinkButton } from "@/components/Buttons/LinkButton";
 import styles from "./style.module.css";
+import { IoMailOutline } from "react-icons/io5";
+import { IoIosSend } from "react-icons/io";
+
 export const TopActions = (): React.ReactNode => {
   return (
     <div className={styles.actions}>
@@ -8,6 +11,7 @@ export const TopActions = (): React.ReactNode => {
         href="/messages/new"
         color="var(--button-primary-color)"
         radius="lg"
+        leftSection={<IoIosSend size={20} />}
       >
         メッセージを書く
       </LinkButton>
@@ -15,6 +19,7 @@ export const TopActions = (): React.ReactNode => {
         href="/messages"
         color="var(--button-primary-color)"
         radius="lg"
+        leftSection={<IoMailOutline size={20} />}
       >
         みんなのメッセージを見る
       </LinkButton>
